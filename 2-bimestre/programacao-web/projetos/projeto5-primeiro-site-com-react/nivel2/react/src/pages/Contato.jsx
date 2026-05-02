a8801946d4ce2cf9d7cb2b505852ebe2ae933213
João Paulo <joao.filho488@aluno.ce.gov.br>
2026-05-02 14:53:32 +0000

    feito2

--- 2-bimestre/programacao-web/projetos/projeto5-primeiro-site-com-react/nivel2/react/src/pages/Contato.jsx
+++ 2-bimestre/programacao-web/projetos/projeto5-primeiro-site-com-react/nivel2/react/src/pages/Contato.jsx
@@ -2,7 +2,7 @@ export default function Contato() {
     return (
         <div>
             <h2>Contato</h2>
-            <p>Email: contato@site.com</p>
+            <p>contato@site.com</p>
             <p>Telefone: (85)992547040</p>
         </div>
     )
