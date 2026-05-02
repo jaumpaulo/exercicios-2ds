4f319f4cdfd4882e60c00f628f0cac015de031d6
João Paulo <joao.filho488@aluno.ce.gov.br>
2026-05-02 14:48:53 +0000

    feito

---
+++ 2-bimestre/programacao-web/projetos/projeto5-primeiro-site-com-react/nivel2/react/src/pages/Contato.jsx
@@ -0,0 +1,9 @@
+export default function Contato() {
+    return (
+        <div>
+            <h2>Contato</h2>
+            <p>Email: contato@site.com</p>
+            <p>Telefone: (85)992547040</p>
+        </div>
+    )
+}
---
+++ 2-bimestre/programacao-web/projetos/projeto5-primeiro-site-com-react/nivel2/react/src/pages/Sobre.jsx
@@ -0,0 +1,9 @@
+export default function Sobre() {
+  return (
+    <div>
+      <h2>Sobre</h2>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur temporibus veritatis fuga. Molestias ex excepturi cumque, pariatur impedit, mollitia dolores porro adipisci accusantium soluta, repudiandae tenetur. Porro, laudantium! Modi, itaque?</p>
+      <p>Somos uma empresa focada em tecnologia.</p>
+    </div>
+  )
+}
