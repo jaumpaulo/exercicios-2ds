8ab785d3ae0144bf4c7f42abcf9cab7c83ffb554
João Paulo <joao.filho488@aluno.ce.gov.br>
2026-05-10 01:41:28 +0000

    feito

--- 2-bimestre/programacao-web/aula07-criando-um-componente-filme/react/src/components/Filme.jsx
+++ 2-bimestre/programacao-web/aula07-criando-um-componente-filme/react/src/components/Filme.jsx
@@ -1,15 +1,9 @@
-export default function Filme() {
-  const filme = {
-    nome: "Matrix",
-    ano: 1999,
-    descricao: "Um hacker descobre a verdade sobre a realidade."
-  }
-
+export default function Filme({ nome, ano, descricao }) {
   return (
     <div>
-      <h1>Nome do filme: {filme.nome}</h1>
-      <h2>Ano do filme: {filme.ano}</h2>
-      <p>{filme.descricao}</p>
+      <h1>Nome do filme: {nome}</h1>
+      <h2>Ano do filme: {ano}</h2>
+      <p>{descricao}</p>
     </div>
   )
 }
