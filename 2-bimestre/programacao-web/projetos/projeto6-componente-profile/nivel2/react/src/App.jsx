921ddabae36dc2188c0950119603ad447d2b778a
João Paulo <shidp65@gmail.com>
2026-05-17 12:02:53 +0000

    Projeto 6 concluido

--- 2-bimestre/programacao-web/projetos/projeto6-componente-profile/nivel2/react/src/App.jsx
+++ 2-bimestre/programacao-web/projetos/projeto6-componente-profile/nivel2/react/src/App.jsx
@@ -1,5 +1,7 @@
 import Profile from "./components/Profile"
 import Header from "./components/Pages/Header"
+import Footer from "./components/Pages/Footer"
+import { UsersRound } from 'lucide-react';
 import "./styles/ProfileCSS.css"
 
 export default function App() {
@@ -7,37 +9,58 @@ export default function App() {
       <div className="container">
         <Header />
 
-        <h1>Usuários do Sistema</h1>
-        <p>Conheça os membros da nossa equipe</p>
+        <div className="apresentaca">
+          <h1>Usuários do Sistema</h1>
+          <p>Conheça os membros da nossa equipe</p>
+        </div>
         
-        <div className="card">
+        <div className="card-container">
           <div className="profile-card">
             <Profile 
-              foto={"https://www.google.com/url?sa=t&source=web&rct=j&url=https%3A%2F%2Fdepositphotos.com%2Fbr%2Fphotos%2Fpessoas-normais.html&ved=0CBYQjRxqFwoTCMCK6Zuut5QDFQAAAAAdAAAAABAG&opi=89978449"}
+              foto={"https://randomuser.me/api/portraits/women/27.jpg"}
               nome={"Mariana Silva"}
               idade={29}
               profissao={"Desenvolvedora Frontend"}
             />
+            <div className="colaboradoes">
+              <UsersRound 
+              style={{color: "blue"}}
+              />
+              <p style={{color: "blue", fontSize: "23px"}}>Colaboradores</p>
+            </div>
           </div>
 
           <div className="profile-card">
             <Profile 
-              foto={"https://www.google.com/url?sa=t&source=web&rct=j&url=https%3A%2F%2Fcajamar.sp.gov.br%2Fnoticias%2F2021%2F07%2F14%2Fvacinacao-contra-a-covid-19-para-pessoas-com-38-anos%2F&ved=0CBYQjRxqFwoTCMCK6Zuut5QDFQAAAAAdAAAAABAh&opi=89978449"}
+              foto={"https://randomuser.me/api/portraits/men/79.jpg"}
               nome={"João Santos"}
               idade={35}
               profissao={"Designer UX/UI"}
             />
+            <div className="colaboradoes">
+              <UsersRound 
+              style={{color: "blue"}}
+              />
+              <p style={{color: "blue", fontSize: "23px"}}>Colaboradores</p>
+            </div>
           </div>
 
           <div className="profile-card">
             <Profile 
-              foto={"https://www.google.com/url?sa=t&source=web&rct=j&url=https%3A%2F%2Fwww.agendartecultura.com.br%2Fnoticias%2Fnegras-lugar-protagonismo-conheca-fotografia-meneson-conceicao%2F&ved=0CBYQjRxqFwoTCMCK6Zuut5QDFQAAAAAdAAAAABA8&opi=89978449"}
+              foto={"https://randomuser.me/api/portraits/men/91.jpg"}
               nome={"Carlos Alberto"}
               idade={41}
               profissao={"Gerente de Projetos"}
             />
+            <div className="colaboradoes">
+              <UsersRound 
+              style={{color: "blue"}}
+              />
+              <p style={{color: "blue", fontSize: "23px"}}>Colaboradores</p>
+            </div>
           </div>
         </div>
+        <Footer />
       </div>
   )
 }
