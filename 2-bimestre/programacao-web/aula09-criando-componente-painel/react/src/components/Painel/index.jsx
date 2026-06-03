bbaa2beb622953c6d918cdd408039be0274cb141
João Paulo <shidp65@gmail.com>
2026-06-03 12:25:33 +0000

    exercicio 9 ajeitado

--- 2-bimestre/programacao-web/aula09-criando-componente-painel/react/src/components/Painel/index.jsx
+++ 2-bimestre/programacao-web/aula09-criando-componente-painel/react/src/components/Painel/index.jsx
@@ -1,15 +1,9 @@
+import "./styles/Painel.css"
+
 export default function Painel({ children }) {
     return(
-            <div
-      style={{
-        border: "2px solid #5d00ff",
-        borderRadius: "10px",
-        padding: "20px",
-        margin: "10px",
-        backgroundColor: "#ffffff",
-      }}
-    >
-      {children}
-    </div>
+      <div className="container">
+        {children}
+      </div>
     )
 }
