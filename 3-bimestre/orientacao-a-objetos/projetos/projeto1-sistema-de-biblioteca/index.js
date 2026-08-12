fb7be21c790120cd1bb473ad86bec0e288089c2a
João Paulo <shidp65@gmail.com>
2026-08-12 13:36:46 +0000

    projeto 1 de POO feito

---
+++ 3-bimestre/orientacao-a-objetos/projetos/projeto1-sistema-de-biblioteca/index.js
@@ -0,0 +1,31 @@
+class Livro {
+    disponivel = true
+
+    constructor(titulo, autor, ano) {
+        this.titulo = titulo
+        this.autor = autor
+        this.ano = ano
+    }
+
+    emprestar() {
+        if(this.disponivel === true) {
+            this.disponivel = false
+        }
+    }
+    
+    devolver() {
+        if(this.disponivel === false) {
+            this.disponivel = true
+        }
+    }
+
+    estaDisponivel() {
+        if(this.disponivel === true) {
+            return this.disponivel
+        } else {
+            return this.disponivel
+        }
+    }
+}
+
+module.exports = Livro
